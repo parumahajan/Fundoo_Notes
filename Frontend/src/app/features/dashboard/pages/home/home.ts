0732a80bb4eabd93d1615b2b0780cb2f81c77027
Pranav Mahajan <parumahajan3000@gmail.com>
2026-01-19 15:47:05 +0530

    feat: Implement label loading in HomeComponent on initialization

--- Frontend/src/app/features/dashboard/pages/home/home.ts
+++ Frontend/src/app/features/dashboard/pages/home/home.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar';
 import { SidebarComponent } from '../../components/sidebar/sidebar';
 import { MainContentComponent } from '../../components/main-content/main-content';
+import { LabelService } from '../../../../core/services/label.service';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +12,17 @@ import { MainContentComponent } from '../../components/main-content/main-content
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private labelService = inject(LabelService);
+
   sidebarExpanded = signal(false);
   isGridView = signal(true);
   searchQuery = signal('');
+
+  ngOnInit(): void {
+    // Load labels when component initializes
+    this.labelService.refreshLabels();
+  }
 
   toggleSidebar(): void {
     this.sidebarExpanded.update(value => !value);
